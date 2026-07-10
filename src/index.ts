9b57773185097d86491734ef93abe31956df01ed
Junior27VQ <favq1999@outlook.com>
2026-07-09 22:25:49 -0500

    Fix: Code corrected and tested from Postman

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,19 @@
 import express from 'express';
+import albumesRoutes from './routes/albumes.js';
+import cancionesRoutes from './routes/canciones.js';
 
 const app = express();
 const PORT = 3001;
 
 app.use(express.json());
+
+app.use('/albums', albumesRoutes);
+app.use('/canciones', cancionesRoutes);
+
 app.use('/catalogos', (req, res) => {
   res.json({ message: 'Ruta de catálogos de música' });
 });
 
-app.use('/albums', (req, res) => {
-  res.json({ message: 'Ruta de albums' });
-});
-
-app.use('/canciones', (req, res) => {
-  res.json({ message: 'Ruta de canciones' });
-});
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
