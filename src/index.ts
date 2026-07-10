ba444ad4626a6b52503fbc248e3d91737cd8a648
Junior27VQ <favq1999@outlook.com>
2026-07-09 20:45:52 -0500

    feat: implemented index

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,14 @@ app.use('/catalogos', (req, res) => {
   res.json({ message: 'Ruta de catálogos de música' });
 });
 
+app.use('/albums', (req, res) => {
+  res.json({ message: 'Ruta de albums' });
+});
+
+app.use('/canciones', (req, res) => {
+  res.json({ message: 'Ruta de canciones' });
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
